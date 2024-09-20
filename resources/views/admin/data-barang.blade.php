1cf9d80ef64bb95d44ae7180cf2a668e4242e428
faanyaya <fanikurniawati123@gmail.com>
2024-09-20 10:28:22 +0700

    update data barang

--- resources/views/admin/data-barang.blade.php
+++ resources/views/admin/data-barang.blade.php
@@ -11,26 +11,24 @@
     @extends('layouts.navbar-admin.data-barang')
 
     @section('navbar-admin')
-    <div class="flex">
-        <div class="w-1/5 p-5 text-white">
+    <div class="flex nav-content">
         </div>
     
-        <div class="ml-96 w-4/5 p-10 fixed">
-            <h1 class="text-5xl font-bold mb-6 mt-0">List Product</h1>
+        <div class="ml-80 w-3/4 p-10 fixed">
+            <h1 class="text-xl font-bold mb-6 mt-0">List Product</h1>
 
-           
             <div class="mb-4 flex justify-end">
-                <button class="bg-blue-500 text-white px-4 py-4 rounded">Add Product</button>
+                <button class="bg-blue-500 text-white px-2 py-2 rounded ml-auto ">Add Product</button>
             </div>
 
-            <div class="">
-                <table class="min-w-full bg-white border">
+            <div  class="overflow-x-auto">
+                <table class="w-full bg-white border">
                     <thead>
                         <tr>
-                            <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm font-semibold text-gray-600">Nama Product</th>
-                            <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm font-semibold text-gray-600">Harga</th>
-                            <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm font-semibold text-gray-600">Kategori</th>
-                            <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm font-semibold text-gray-600">Aksi</th>
+                            <th class="px-6 py-3 border-b-2 border-gray-300 text-center text-sm font-semibold text-gray-600">Nama Product</th>
+                            <th class="px-6 py-3 border-b-2 border-gray-300 text-center text-sm font-semibold text-gray-600">Harga</th>
+                            <th class="px-6 py-3 border-b-2 border-gray-300 text-center text-sm font-semibold text-gray-600">Kategori</th>
+                            <th class="px-6 py-3 border-b-2 border-gray-300 text-center text-sm font-semibold text-gray-600">Aksi</th>
                         </tr>
                         </thead>
         </div>
